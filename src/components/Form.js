03d785eba53bacdc17e436de9672eef7de882994
Jexeones24 <jexeones@gmail.com>
2017-11-16 15:52:53 -0500

    updated README

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,19 +5,23 @@ export default class Form extends Component {
     return (
       <div className='form'>
         <div className={this.props.className}>
-          <label>{this.props.label}</label>
-          <form>
-            {this.props.inputs.map((field, idx) =>
-              <Input
-                key={idx}
-                type={this.props.type}
-                name={this.props.name}
-                value=''
-                placeholder={field}
-              />
-            )}
-            <button className='btn-submit'>SUBMIT</button>
-          </form>
+          <div className='form-label'>
+            <label>{this.props.label}</label>
+          </div>
+          <div className='form-body'>
+            <form>
+              {this.props.inputs.map((field, idx) =>
+                <Input
+                  key={idx}
+                  type={this.props.type}
+                  name={this.props.name}
+                  value=''
+                  placeholder={field}
+                />
+              )}
+              <button className='btn-submit'>SUBMIT</button>
+            </form>
+          </div>
         </div>
       </div>
     )
@@ -28,7 +32,6 @@ const Input = (props) => {
   return (
     <div>
       <input
-        key={props.key}
         type={props.type}
         name={props.name}
         value={props.value}
